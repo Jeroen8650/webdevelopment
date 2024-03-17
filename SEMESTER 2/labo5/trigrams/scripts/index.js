235ba355295a73ab22413594fe48d84f70ede200
Seppe Igodt <seppe.igodt@student.vives.be>
2024-03-17 11:31:54 +0100

    last minute aanpassingen

---
+++ SEMESTER 2/labo5/trigrams/scripts/index.js
@@ -0,0 +1,18 @@
+
+const setup = () => {
+
+    let btnBewerk = document.getElementById("btnBewerk");
+    btnBewerk.addEventListener("click", bewerk);
+}
+const bewerk = () => {
+    let txtInput = document.getElementById("txtInput").value;
+    txtInput = txtInput.replaceAll(" ", "");
+    let getal = 0;
+    while (getal < txtInput.length){
+        if (txtInput.substring(getal,3+getal).length === 3){
+            console.log(txtInput.substring(getal,3+getal))
+        }
+        getal += 1;
+    }
+}
+window.addEventListener("load", setup);
